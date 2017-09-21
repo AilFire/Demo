08a7914950e498f13341cab3b8d4cf6f7e558124
AilFire <2902207934@qq.com>
2017-09-21 20:07:41 +0800

    Attribute to use

---
+++ Demo/Attribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Index
+{
+//1.Attribute;
+}
+namespace Demo
+{
+    [AttributeUsage(AttributeTargets.All,/*使用特性的途径*/Inherited = true,/*是否允许被继承;*/AllowMultiple =true)]//AllowMultiple获取或设置一个布尔值，该值指示能否为一个程序元素指定多个指示属性实例
+    class DemoAttribute:Attribute   //特性,使用的时候直接使用名字Demo,特性会自动补全;
+    {
+        DemoAttribute() { }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Demo
 {
     class Program
     {
-      
-        static void Main(string[] args)
+        static void Main()
         {
-            
+            Console.ReadKey();
         }
     }
 }
